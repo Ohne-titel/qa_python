f50771ac2fbfe4810de49b5d23d2652c875daee3
Elina Myagkova <miagkova.elina@yandex.ru>
2024-04-25 16:00:47 +0300

    added test for get_book_genre method (test_get_book_genre_added_book_has_no_genre)

--- tests.py
+++ tests.py
@@ -32,11 +32,6 @@ class TestBooksCollector:
         actual_result = books_collector.get_book_genre(name) is not None
         assert actual_result == expected_result
 
-    def test_add_book_added_book_has_no_genre(self):
-        books_collector = BooksCollector()
-        books_collector.add_new_book('Война и мир')
-        assert books_collector.get_book_genre('Война и мир') == ''
-
     def test_set_book_genre(self):
         books_collector = BooksCollector()
         name = 'Гарри Поттер и принц полукровка'
@@ -45,11 +40,10 @@ class TestBooksCollector:
         books_collector.set_book_genre(name, genre)
         assert books_collector.get_book_genre('Гарри Поттер и принц полукровка') == 'Фантастика'
 
-    def test_get_book_genre(self):
+    def test_get_book_genre_added_book_has_no_genre(self):
         books_collector = BooksCollector()
-        books_collector.add_new_book('Гарри Поттер и принц полукровка')
-        books_collector.set_book_genre('Гарри Поттер и принц полукровка', 'Фантастика')
-        assert books_collector.get_book_genre('Гарри Поттер и принц полукровка') == 'Фантастика'
+        books_collector.add_new_book('Война и мир')
+        assert books_collector.get_book_genre('Война и мир') == ''
 
     def test_get_books_with_specific_genre(self):
         books_collector = BooksCollector()
@@ -58,13 +52,6 @@ class TestBooksCollector:
         books_collector.add_new_book('Ночь в Люберцах')
         books_collector.set_book_genre('Ночь в Люберцах', 'Ужасы')
         assert books_collector.get_books_with_specific_genre('Ужасы') == ['Ночь в Люберцах']
-
-    def test_get_books_genre(self):
-        books_collector = BooksCollector()
-        books_collector.add_new_book('Азазель')
-        books_collector.set_book_genre('Азазель', 'Детектив')
-        books_collector.add_new_book('Азазель')
-        books_collector.set_book_genre('Азазель', 'Детектив')
 
     def test_get_books_for_children(self):
         books_collector = BooksCollector()
