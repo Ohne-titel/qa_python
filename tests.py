9e9450b4d9391a00617e0f6650d015235d977000
Elina Myagkova <miagkova.elina@yandex.ru>
2024-04-25 15:51:35 +0300

    changed test for set_book_genre

--- tests.py
+++ tests.py
@@ -37,13 +37,13 @@ class TestBooksCollector:
         books_collector.add_new_book('Война и мир')
         assert books_collector.get_book_genre('Война и мир') == ''
 
-    # def test_set_book_genre(self):
-    #     books_collector = BooksCollector()
-    #     name = 'Гарри Поттер и принц полукровка'
-    #     genre = 'Фантастика'
-    #     books_collector.add_new_book(name)
-    #     books_collector.set_book_genre(name, genre)
-    #     assert books_collector.set_book_genre(name, genre) == genre
+    def test_set_book_genre(self):
+        books_collector = BooksCollector()
+        name = 'Гарри Поттер и принц полукровка'
+        genre = 'Фантастика'
+        books_collector.add_new_book(name)
+        books_collector.set_book_genre(name, genre)
+        assert books_collector.get_book_genre('Гарри Поттер и принц полукровка') == 'Фантастика'
 
     def test_get_book_genre(self):
         books_collector = BooksCollector()
@@ -58,6 +58,13 @@ class TestBooksCollector:
         books_collector.add_new_book('Ночь в Люберцах')
         books_collector.set_book_genre('Ночь в Люберцах', 'Ужасы')
         assert books_collector.get_books_with_specific_genre('Ужасы') == ['Ночь в Люберцах']
+
+    def test_get_books_genre(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Азазель')
+        books_collector.set_book_genre('Азазель', 'Детектив')
+        books_collector.add_new_book('Азазель')
+        books_collector.set_book_genre('Азазель', 'Детектив')
 
     def test_get_books_for_children(self):
         books_collector = BooksCollector()
